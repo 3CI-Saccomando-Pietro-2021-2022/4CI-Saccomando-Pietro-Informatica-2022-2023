05ccb89d7bfa924430fec3987488b43ddeb8c7c2
4CI-Saccomando-Pietro-2022-2023 <91484826+4CI-Saccomando-Pietro-2022-2023@users.noreply.github.com>
2022-11-17 18:49:16 +0100

    Delete 2022-2023/bin directory

--- 2022-2023/bin/ApplicazioneArticolo.java
+++
@@ -1,6 +0,0 @@
-public class ApplicazioneArticolo{
-    public static void main(String args[]){
-        VenditaArticolo vendiArticolo= new VenditaArticolo();
-        vendiArticolo.start();
-    }
-}
